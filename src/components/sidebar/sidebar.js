757fd7e6d6aa78acc8eba64babec472206a3a8ee
AshCryptsCapts <sahithialluri22@gmail.com>
2023-05-20 23:08:01 +0530

    crud 0.2

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -12,7 +12,23 @@ export default function Sidebar() {
     return (
 
         <div className="container sidebar">
-            <div className="row ">
+            <div className='sidebarHead'>
+                <i class="fa-brands fa-slack"></i>
+                <h2 style={{margin : 0}}>Stealth</h2>
+            </div>
+            <hr />
+            <div className='sidebarBody'>
+            <ul>
+                <li >
+                    <i className="fa-solid fa-file"></i>
+                    <h5>
+                        <i className="fs-4 bi-house"></i> <span className="ms-1 d-none d-sm-inline">File Manager</span>
+                    </h5>
+                </li>
+            </ul>
+            </div>
+            
+            {/* <div className="row ">
                 <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0">
                     <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                         <div className='sidebarHead'>
@@ -21,6 +37,7 @@ export default function Sidebar() {
                                Stealth
                             </h2>
                         </div>
+                        <hr />
 
                         <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                             <li >
@@ -32,7 +49,7 @@ export default function Sidebar() {
                         </ul>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/pages/filemanager/filemanager.js
+++ src/pages/filemanager/filemanager.js
@@ -1,17 +1,16 @@
 import React, { useContext, useState } from 'react'
 
 import './filemanager.css'
-// import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined';
-// import DeleteIcon from '@mui/icons-material/Delete';
+
 import Categories from './categories'
 
-// import { useGlobalContext } from '../../utils/context'
+
 import { AppContext } from '../../utils/context';
-// import DeleteConfirmationModal from './deleteConfimation';
+
 
 
 export default function Filemanager() {
-  // const data = useGlobalContext;
+
   const { data, isLoading, error, deleteItem } = useContext(AppContext);
   const [checkedItems, setCheckedItems] = useState([]);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
@@ -24,25 +23,12 @@ export default function Filemanager() {
   if (error) {
     return <p>Error: {error.message}</p>;
   }
-  //random label code 0.1
+
   const generateLabel = (id) => {
     const labelNumber = id % 6 + 1;
     return `Label ${labelNumber}`;
   };
 
-  // const generateRandomLabel = () => {
-  //   const labels = ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5', 'Label 6'];
-  //   const randomIndex = Math.floor(Math.random() * labels.length);
-  //   return labels[randomIndex];
-  // };
-  // const filteredData = selectedLabel
-  //   ? data.filter((item) => generateRandomLabel() === selectedLabel)
-  //   : data;
-
-  //basic handle delete
-  // const handleDelete = (itemId) => {
-  //   deleteItem(itemId);
-  // };
 
   const handleDelete = (itemId) => {
     setShowDeleteModal(true);
@@ -87,7 +73,6 @@ export default function Filemanager() {
           <nav class="navbar" style={{ "border-bottom": 0 }}>
             <div class="container" style={{ "justify-content": "flex-end" }}>
               <form class="d-flex">
-                {/* <i class="fa fa-search" aria-hidden="true"></i> */}
                 <label className="search" > <i class="fa fa-search" aria-hidden="true"></i> </label>
                 <input class="form-control me-2 " type="search" placeholder="   Search by name" aria-label="Search" />
               </form>
@@ -99,27 +84,20 @@ export default function Filemanager() {
             </div>
             <div className='col-md-9 tableAllItems'>
               <div className='tableHead'><h5>All Items</h5></div>
-              
-              {/* {showDeleteModal && (
-        <DeleteConfirmationModal
-          onDeleteConfirm={handleDeleteConfirm}
-          onDeleteCancel={handleDeleteCancel}
-        />
-      )} */}
+
               <div className="modal fade" show={showDeleteModal.toString()} id="deleteConfirm" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div className="modal-dialog">
                   <div className="modal-content">
                     <div className="modal-header">
-                      <h5 className="modal-title" id="staticBackdropLabel">Modal title</h5>
+                      <h5 className="modal-title" id="staticBackdropLabel">Confirm Delete?</h5>
                       <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                      <h5>Popover in a modal</h5>
-                      <p>This <a href="/" role="button" className="btn btn-secondary popover-test" title="Popover title" data-bs-content="Popover body content is set in this attribute.">button</a> triggers a popover on click.</p>
+                      <h5>Are you sure you want to delete</h5>
                     </div>
-                    <div className="modal-actions">
-                      <button onClick={handleDeleteConfirm}>Delete</button>
-                      <button data-bs-dismiss="modal" onClick={handleDeleteCancel}>Cancel</button>
+                    <div className="modal-actions" style={{"display": "flex","justify-content":" space-evenly","align-items": "center","padding":"15px"}}>
+                    <button type="button" class="btn btn-danger" onClick={handleDeleteConfirm}>Delete</button>
+                    <button type="button" class="btn btn-success" data-bs-dismiss="modal" onClick={handleDeleteCancel}>Cancel</button>
                     </div>
                   </div>
                 </div>
@@ -145,7 +123,7 @@ export default function Filemanager() {
                         <td className='fileNameFeild'><img className='fileImg' src={items.file} alt='file' /> &nbsp;  &nbsp; {items.Name}</td>
                         <td><img className='ownerImg' src={items.Owner} alt='Owner' /></td>
                         <td><label htmlFor={`label-${items.id}`}>{generateLabel(items.id)}</label></td>
-                        {/* <td>{generateRandomLabel()}</td> */}
+
                         <td>{items.Type}</td>
                         <td>{items.ModifietAt}</td>
                         <td><button className="btn"><i class="fa-regular fa-pen-to-square"></i></button>
@@ -163,15 +141,6 @@ export default function Filemanager() {
                   <hr />
                 </tbody>
               </table>
-              {/* <div className="row row-cols-7 mainRow">
-              <div className="col-md-2"><input type="checkbox" id="selectAll" name="selectAll" value="selectAll" /> </div>
-              <div className="col-md-2">Name</div>
-              <div className="col-md-2">Owner</div>
-              <div className="col-md-2">Labels</div>
-              <div className="col-md-2">Type</div>
-              <div className="col-md-2">Modified</div>
-              <div className="col-md-2">Action</div>
-            </div> */}
             </div>
           </div>
         </div>
